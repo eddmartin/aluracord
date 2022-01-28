b4ed9c3f620d3abe8108b6f6eb5ab108931b7eb1
Edival Martin <edival.martin@gmail.com>
2022-01-27 22:06:33 -0300

    Class 2

---
+++ pages/chat.js
@@ -0,0 +1,13 @@
+export default function PaginaDoChat() {
+    return (
+        <>
+        <div>"Doidera"</div>
+        <style jsx>{`
+            * {
+                background-color: lime;
+            }
+        `}
+        </style>
+        </>
+    )
+}
